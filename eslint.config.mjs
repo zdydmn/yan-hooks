86123289b50b316fe47eb958aab1e4d5af280155
zhangYY <zhangyy@zhangYYdeMac-mini.local>
2025-04-14 07:05:51 +0800

    feat: 修改构建工具

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,4 @@
-import { fixupConfigRules } from '@eslint/compat';
 import js from '@eslint/js';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactJsx from 'eslint-plugin-react/configs/jsx-runtime.js';
-import react from 'eslint-plugin-react/configs/recommended.js';
 import globals from 'globals';
 import ts from 'typescript-eslint';
 
@@ -10,22 +6,5 @@ export default [
   { languageOptions: { globals: globals.browser } },
   js.configs.recommended,
   ...ts.configs.recommended,
-  ...fixupConfigRules([
-    {
-      ...react,
-      settings: {
-        react: { version: 'detect' },
-      },
-    },
-    reactJsx,
-  ]),
-  {
-    plugins: {
-      'react-hooks': reactHooks,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-    },
-  },
   { ignores: ['dist/'] },
 ];
